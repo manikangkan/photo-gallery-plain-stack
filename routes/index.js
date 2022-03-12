9ef3b020fe8b12402fdf80b94b14808dd079578d
manikangkandas global <manikangkan.das@yahoo.com>
2022-03-12 23:41:50 +0530

    initial commit

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,10 @@ const router = Router();
 const cloudinary = require("../utils/cloudinary");
 const upload = require("../utils/multer");
 const Image = require("../models/Image");
+const path = require("path");
 
 router.get("/", async (req, res) => {
-  const result = await Image.find();
+  const result = await Image.find().sort({ createdAt: -1 });
   let collection = [];
   result.forEach((data) => {
     collection.push({
@@ -20,13 +21,13 @@ router.get("/", async (req, res) => {
 });
 
 router.get("/admin", async (req, res) => {
-  const result = await Image.find();
+  const result = await Image.find().sort({ createdAt: -1 });
   let collection = [];
   result.forEach((data) => {
     collection.push({
       _id: data._id,
-      title: data.title,
-      description: data.description,
+      title: data.title.slice(0, 10),
+      description: data.description.slice(0, 10),
       imageURL: data.imageURL,
       public_id: data.public_id,
     });
@@ -53,7 +54,7 @@ router.post("/admin", upload.single("image"), async (req, res) => {
 });
 router.get("/delete/:id", upload.single("image"), async (req, res) => {
   const { id } = req.params;
-  const image = await Photo.findByIdAndDelete(id);
+  const image = await Image.findByIdAndDelete(id);
   const result = await cloudinary.uploader.destroy(image.public_id);
   console.log(result);
   res.redirect("/admin");
