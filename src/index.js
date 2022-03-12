83e789d7eaa329a4335b45d63593b9af0f2832d4
manikangkandas global <manikangkan.das@yahoo.com>
2022-03-12 18:35:07 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+if (process.env.NODE_ENV !== 'production') {
+	require('dotenv').config()
+}
+
+const app = require('./app')
+
+app.listen(app.get('port'), () => {
+	console.log(`Server on port ${app.get('port')}`)
+})
